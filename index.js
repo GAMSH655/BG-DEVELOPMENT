83079897aa20dc29845210e34f55ef5605c84ae8
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-01-03 12:04:30 -0800

    making the navbar visible with the press of hamburger menu and cancelling the visibillity with the cancel buton

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const lists= document.getElementsByClassName('list')
-console.log (lists)
+const barBtn = document.querySelector('.fa-bars')
+const navList = document.querySelector(".list")
+const cancelBtn =document.querySelector (".cancelbtn")
+
+barBtn.addEventListener('click' , function () {
+     console.log('click')
+     navList.style.display = "block"
+})
+
+cancelBtn.addEventListener('click' , function () {
+    console.log('click')
+    navList.style.display = "none"
+})
